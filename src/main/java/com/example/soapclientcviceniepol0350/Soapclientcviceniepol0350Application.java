a70ff0a645709353387d99e432e639a87404b133
Patrick <polacek.patrik@gmail.com>
2020-03-02 00:00:11 +0100

    added files

---
+++ src/main/java/com/example/soapclientcviceniepol0350/Soapclientcviceniepol0350Application.java
@@ -0,0 +1,13 @@
+package com.example.soapclientcviceniepol0350;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Soapclientcviceniepol0350Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Soapclientcviceniepol0350Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/soapclientcviceniepol0350/Soapclientcviceniepol0350ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.soapclientcviceniepol0350;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Soapclientcviceniepol0350ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
